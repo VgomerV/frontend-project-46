0bdfcb9bb6933ae837c720643774ab8105b8f32f
Gomer <vgomer@list.ru>
2024-08-27 23:52:55 +0300

    step 7 completed

--- Makefile
+++ Makefile
@@ -8,3 +8,9 @@ lint:
 
 test:
 	NODE_OPTIONS=--experimental-vm-modules npx jest
+
+startJ:
+	node bin/gendiff __fixtures__/file1.json __fixtures__/file2.json
+
+startY:
+	node bin/gendiff __fixtures__/file1.yaml __fixtures__/file2.yaml
--- src/index.js
+++ src/index.js
@@ -1,33 +1,82 @@
 import _ from 'lodash';
 import parsing from './parsing.js';
+import formatter from './formatters.js';
 
-export default (path1, path2) => {
-  const extension = path1.split('.')[0];
+const parsingObject = (obj, flag) => Object.keys(obj)
+  .map((key) => {
+    const value = obj[key];
 
-  const dataFromFile1 = parsing(path1, extension);
-  const dataFromFile2 = parsing(path2, extension);
+    return [key, !_.isObject(value) ? value : parsingObject(value, flag), flag];
+});
 
-  const keys1 = Object.keys(dataFromFile1);
-  const keys2 = Object.keys(dataFromFile2);
+const genDiff = (file1, file2, format) => {
+  const extension = file1.split('.')[1];
 
-  const unionArray = _.union(keys1, keys2)
-    .sort((a, b) => a.localeCompare(b));
+  const dataFromFile1 = parsing(file1, extension);
+  const dataFromFile2 = parsing(file2, extension);
 
-  const result = unionArray.reduce((acc, key) => {
-    if (!Object.hasOwn(dataFromFile1, key)) {
-      acc.push(`  + ${key}: ${dataFromFile2[key]}\n`);
-    } else if (!Object.hasOwn(dataFromFile2, key)) {
-      acc.push(`  - ${key}: ${dataFromFile1[key]}\n`);
-    } else if (dataFromFile1[key] === dataFromFile2[key]) {
-      acc.push(`    ${key}: ${dataFromFile1[key]}\n`);
-    } else {
-      acc.push(`  - ${key}: ${dataFromFile1[key]}\n`);
-      acc.push(`  + ${key}: ${dataFromFile2[key]}\n`);
-    }
+  const iter = (file1, file2) => {
+    const keysFromFile1 = Object.keys(file1);
+    const keysFromFile2 = Object.keys(file2);
 
-    return acc;
-  }, ['{\n']);
+    const keys = _.union(keysFromFile1, keysFromFile2)
+      .sort((a, b) => a.localeCompare(b));
 
-  result.push('}');
-  return result.join('');
+    const dataForFormatter = keys.reduce((acc, key) => {
+      const hasFile1Key = Object.hasOwn(file1, key);
+      const hasFile2Key = Object.hasOwn(file2, key);
+
+      const value1 = file1[key];
+      const value2 = file2[key];
+
+      const value1IsObject = _.isObject(value1);
+      const value2IsObject = _.isObject(value2);
+
+      if (!hasFile1Key) {
+        acc.push([key, !value2IsObject ? value2 : parsingObject(value2, ' '), '+']);
+        return acc;
+      }
+
+      if (!hasFile2Key) {
+        acc.push([key, !value1IsObject ? value1 : parsingObject(value1, ' '), '-']);
+        return acc;
+      }
+
+      if (_.isEqual(value1, value2)) {
+        acc.push([key, !value1IsObject ? value1 : parsingObject(value1, ' '), ' ']);
+        return acc;
+      }
+
+      if (!value1IsObject && !value2IsObject) {
+        acc.push([key, value1, '-']);
+        acc.push([key, value2, '+']);
+        return acc;
+      }
+
+      if (!value1IsObject && value2IsObject) {
+        acc.push([key, value1, '-']);
+        acc.push([key, parsingObject(value1, ' '), '+']);
+        return acc;
+      }
+
+      if (value1IsObject && !value2IsObject) {
+        acc.push([key, parsingObject(value1, ' '), '-']);
+        acc.push([key, value2, '+']);
+        return acc;
+      }
+
+      if (value1IsObject && value2IsObject) {
+        acc.push([key, iter(value1, value2), ' ']);
+        return acc;
+      }
+
+      return acc;
+    }, []);
+
+    return dataForFormatter;
+  };
+
+  return formatter(iter(dataFromFile1, dataFromFile2), format);
 };
+
+export default genDiff;
