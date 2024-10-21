8553088cce95678d6d7986152de882b1aed1939f
Gomer <vgomer@list.ru>
2024-10-21 22:25:39 +0300

    fix bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,7 +10,6 @@ program
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format: stylish, plain, json', 'stylish')
-  .allowUnknownOption()
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2, program.opts().format));
   })
