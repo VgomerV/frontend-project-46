2231b7270991d0813b84c1dada5a8cd873efc17a
Gomer <vgomer@list.ru>
2024-10-22 01:08:27 +0300

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,6 +11,6 @@ const param = [
   ['yaml', 'json'],
 ];
 
-test.each(param)('test genDiff, format - %s, formatter - %s', (format, formatter) => {
+test.each(param)('test genDiff, format - %s, formatter - %i', (format, formatter) => {
   expect(genDiff(`__fixtures__/file1.${format}`, `__fixtures__/file2.${format}`, `${formatter}`)).toEqual(readFileSync(path.resolve('__fixtures__/', `result-${formatter}.txt`), 'utf-8'));
 });
